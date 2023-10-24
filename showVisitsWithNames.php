bfea94e5cdd7be77fe6024af960cba14b27c849a
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2023-10-24 11:01:12 +0100

    added showVisits.php and showVisitsWithNames.php

---
+++ showVisitsWithNames.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+      <title>Bootstrap Example</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+      <div class="container mt-3">
+        <h2>Patient Visits</h2>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Visit Date</th>
+                <th>Visit Time</th>
+                <th>Firstname</th>
+                <th>Surname</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php include("visitListWithNames.php");?>
+            </tbody>
+          </table>
+       </div>
+    </body>
+</html>
